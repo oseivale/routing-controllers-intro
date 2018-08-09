f753006f8171a7f55d9a95bfcb1b7188adbee425
Valerie Osei <valerie.osei@gmail.com>
2018-08-09 15:56:25 -0400

    created method inside of the controller, tested the routes with newly defined controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # This is where we define our routes for this Rails app
+
+get '/welcome' => 'pages#welcome'
+# The above line of code states that if the user makes a GET request to the URL "/welcome",
+# Rails should run the welcome instance method inside the PagesController
+
+get '/' => 'pages#welcome'
+
 end
